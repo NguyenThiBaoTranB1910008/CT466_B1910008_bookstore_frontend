19d2aefff2b77be68f7cfd42507d82a742599fcd
baotran9 <baotran096b@gmail.com>
2022-12-31 14:21:03 +0700

    Khoi tao du an

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
+import React, { useState } from 'react';
+import { Routes, Route } from "react-router-dom"
+import AppHeader from './components/common/header/AppHeader'
+import AppFooter from './components/AppFooter';
+import Home from './views/Home';
+import Category from './components/Category'
+import AppAlert from './components/alert/AppAlert';
+import Detail from './views/Detail'
+import { useEffect } from "react";
+import ProductService from './services/product.service'
+import { Link } from "react-router-dom"
+import Cart from './views/Cart.jsx'
 function App() {
+  const [books, setBooks] = useState([])
+    useEffect(() => {
+        async function fetchData(){
+                    try {
+                        const apibooks = await ProductService.getAll();
+                        setBooks(apibooks)
+                        
+                    } catch (error) {
+                        console.log(error);
+                    }
+        }
+        fetchData()
+    },[]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AppHeader/>
+        <Routes>
+          <Route path="/" element={<Home books={books}/>}/>
+          <Route path="/product/:id" element={<Detail/>}/>
+          <Route path="/cart" element={<Cart/>}/>
+        </Routes>
+      <AppFooter/>
+    </>
   );
 }
 
